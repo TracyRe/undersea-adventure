1fe49ca2faade8585be0b921047c514dc49c02c1
Maly Phongsavanh and Tracy Jo Reith <maly@>
2019-03-05 16:20:45 -0800

    add fade in and out with addClass function in JS file

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#big-eye-tuna").click(function(){
+    $(".info-card").addClass("fade-in");
+    $(".info-card").toggle();
+  })
+
+
+
+  // $(".info-card").click(function(){
+  //   $(".info-card").addClass("fade-out");
+  //   $(".info-card").hide();
+  // })
+
+});
