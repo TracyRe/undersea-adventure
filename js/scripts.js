524015602d7466e947ad18f6e553d632f4e61968
Maly Phongsavanh and Tracy Jo Reith <tracy@>
2019-03-06 16:25:00 -0800

    change size mixin

--- js/scripts.js
+++ js/scripts.js
@@ -18,43 +18,62 @@ $(document).ready(function() {
     $(".info-card").children().hide()
     $(".info-card").fadeIn(600);
     $("#info-octopus").show();
-});
+  });
+  $("#puffer").click(function(){
+    $(".info-card").children().hide()
+    $(".info-card").fadeIn(600);
+    $("#info-puffer").show();
+  });
+  $("#razorfish").click(function(){
+    $(".info-card").children().hide()
+    $(".info-card").fadeIn(600);
+    $("#info-razorfish").show();
+  });
+  $("#great-white").click(function(){
+    $(".info-card").children().hide()
+    $(".info-card").fadeIn(600);
+    $("#info-great-white").show();
+  });
   $(".info-card-container").click(function() {
     $(".info-card").fadeOut(800);
   });
 
 
-	// init controller
-	var controller = new ScrollMagic.Controller();
+  // init controller
+  var controller = new ScrollMagic.Controller();
 
-	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: scroll })
-					.addTo(controller)
-					// .addIndicators() // add indicators (requires plugin)
-					// .on("update", function (e) {
-					// 	$("#scrollDirection").text(e.target.controller().info("scrollDirection"));
-					// })
-					// .on("enter leave", function (e) {
-					// 	$("#state").text(e.type == "enter" ? "inside" : "outside");
-					// })
-					// .on("start end", function (e) {
-					// 	$("#lastHit").text(e.type == "start" ? "top" : "bottom");
-					// })
-					.on("progress", function (e) {
-            var meters = (e.progress.toFixed(3) * 3688).toFixed(0);
-
-            if (meters > 0 && meters < 1000) {
-              $(".depth-text").text("100 M");
-            } else if (meters >= 1000 && meters <= 2499) {
-                $(".depth-text").text("500 M");
-            } else if (meters >= 2500) {
-                $(".depth-text").text("1000 M");
-              }
-            });
+  // build scene
+  var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: scroll })
+  .addTo(controller)
+  // .addIndicators() // add indicators (requires plugin)
+  // .on("update", function (e) {
+  // 	$("#scrollDirection").text(e.target.controller().info("scrollDirection"));
+  // })
+  // .on("enter leave", function (e) {
+  // 	$("#state").text(e.type == "enter" ? "inside" : "outside");
+  // })
+  // .on("start end", function (e) {
+  // 	$("#lastHit").text(e.type == "start" ? "top" : "bottom");
+  // })
+  .on("progress", function (e) {
+    var meters = (e.progress.toFixed(3) * 3688).toFixed(0);
 
 
-
-
-
+    if ( meters > 2900 ) {
+      $(".depth-text").text("3000 m");
+    } else if ( meters > 2500 ) {
+      $(".depth-text").text("2500 m");
+    } else if ( meters > 2100 ) {
+      $(".depth-text").text("2000 m");
+    } else if ( meters > 1700 ) {
+      $(".depth-text").text("1500 m");
+    } else if ( meters > 1400 ) {
+      $(".depth-text").text("1000 m");
+    } else if ( meters > 1000 ) {
+      $(".depth-text").text("500 m");
+    } else {
+      $(".depth-text").text("100 m");
+    }
+  });
 
 });
